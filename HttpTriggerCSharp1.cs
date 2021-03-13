498e50be0033431c91df74393b86c345d3741cd4
Joao Meira <39927254+joaomeirayahoocom@users.noreply.github.com>
2021-03-13 10:14:51 -0500

    Add files via upload

---
+++ HttpTriggerCSharp1.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class HttpTriggerCSharp1
+    {
+        [FunctionName("HttpTriggerCSharp1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            
+
+            return new OkObjectResult("testtest");
+        }
+    }
+}
+
