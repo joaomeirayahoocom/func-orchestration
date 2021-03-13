7a518dcbb17b6b2de2b4ca88b66bfcc3b6e632fb
Joao Meira <39927254+joaomeirayahoocom@users.noreply.github.com>
2021-03-13 01:52:53 -0500

    Add files via upload

---
+++ FuncDeployDemo.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class FuncDeployDemo
+    {
+        [FunctionName("FuncDeployDemo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("Awesome app version 1.1.");
+        }
+    }
+}
+
